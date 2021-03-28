4973292522a508d0a69d95c614fe6f349c561efc
Johan Castillo <jcjohan2707@gmail.com>
2021-03-28 17:48:18 -0400

    Send mail new app

--- contacto/correo.php
+++ contacto/correo.php
@@ -1,23 +1,21 @@
 <?php
-        //En el destino colocar el correo alque quieres que lleguen los datos del contacto de tu formulario
-    if(isset($_POST['name'])){
-        
-        $destino = "jcjohan2707@gmail.com";
-        
-        $nombre = $_POST["name"];
-        
-        
-        $email = $_POST["email"];
-        
-        $numero = $_POST["numero"];
-        
-        $mensaje = $_POST["message"];
-        
-        $contenido = "Nombre: " . $nombre . "\nCorreo: " . $email . "\nNúmero: " . $numero . "\nAsunto: " . $message;
-        
-        mail($destino, "Contacto", $contenido);
 
-        header("Location: index.php");
-    }
-//Esto es opcional, aqui pueden colocar un mensaje de "enviado correctamente" o redireccionarlo a algun lugar
+    // Correo al que se enviará el mensaje
+    $detinatario = "jcjohan2707@gmail.com";
+
+    $name = $_POST['name'];
+    $asunto = $_POST['title'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    $email = $_POST['email'];
+
+    $header = "Nuevo pedido de Mixtura";
+
+    $messageToSend = $message . "/nAtentamemnte: " . $name;
+
+    mail($detinatario, $asunto, $messageToSend, $header);
+
+    echo '<script>alert("Mensaje enviado correctamente")</script>';
+    echo '<script>setTimeout(() => location.href = "/", 1000)</script>';
+
 ?>
